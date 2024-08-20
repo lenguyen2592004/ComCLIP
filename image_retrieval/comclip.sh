c2ae4fb5950c25213ca598a318daf27970e16001
Pham Hoang Le Nguyen <120765515+lenguyen2592004@users.noreply.github.com>
2024-08-20 23:31:58 +0700

    Update comclip.sh

--- image_retrieval/comclip.sh
+++ image_retrieval/comclip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-$1 image_folder
-$2 grit densecaption result
-$3 grit model path
-$4 data_csv path
-$5 openai API key
-$6 vision encoder vision
+#$1 image_folder
+#$2 grit densecaption result
+#$3 grit model path
+#$4 data_csv path
+#$5 openai API key
+#$6 vision encoder vision
 mkdir $2
 python GRiT/demo.py --test-task DenseCap --config-file GRiT/configs/GRiT_B_DenseCap_ObjectDet.yaml  --input $1 --output $2 --opts MODEL.WEIGHTS $3
 mkdir image_retrieval/relation_json
