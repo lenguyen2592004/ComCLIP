9a9b22eaca36303c052b9e88a3c0591bb1e9a36a
Pham Hoang Le Nguyen <120765515+lenguyen2592004@users.noreply.github.com>
2024-08-21 12:16:36 +0700

    Update blip2.py

--- image_retrieval/blip2.py
+++ image_retrieval/blip2.py
@@ -15,8 +15,8 @@ args = parser.parse_args()
 #GRiT/configs/GRiT_B_DenseCap_ObjectDet.yaml
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 data = pd.read_pickle('./ComCLIP/datasets/flickr30k_test.pkl') ### Flickr30k or MSCOCO test set
-preprocess= Blip2Processor.from_pretrained('Salesforce/blip2-opt-2.7b',device=device)
-model = Blip2Model.from_pretrained('Salesforce/blip2-opt-2.7b',device=device)
+preprocess= Blip2Processor.from_pretrained('Salesforce/blip2-opt-2.7b')
+model = Blip2Model.from_pretrained('Salesforce/blip2-opt-2.7b')
 model.cuda(device).eval()
 IMAGE_PATH = "./ComCLIP/GRiT/demo_images/000000353174" + "{}.jpg"
 TEXT_JSON_PATH = "" + "/{}.json"
