f4b19cfbe18a4b552a609fb1345ba40a39340670
Pham Hoang Le Nguyen <120765515+lenguyen2592004@users.noreply.github.com>
2024-08-21 21:50:27 +0700

    Update blip2.py

--- image_retrieval/blip2.py
+++ image_retrieval/blip2.py
@@ -115,7 +115,7 @@ def get_score(row_id):
 
 if __name__ == "__main__":
     comclip_score = {}
-    for idx, row in data.iterrows():
+    for idx, row in data.items():
         try:
             comclip_score[idx] = create_sub_image_obj(idx)
             print("Anh thu {}:,".format(idx,comclip_score[idx]))
